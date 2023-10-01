066d4a6672135cf10c59286931f82ddbf9843154
Kleadron <kleadron04@gmail.com>
2023-09-30 21:16:35 -0700

    GLES1 MultiMesh Element should have default matrix

--- drivers/gles1/rasterizer_gles1.h
+++ drivers/gles1/rasterizer_gles1.h
@@ -350,6 +350,27 @@ class RasterizerGLES1 : public Rasterizer {
 
 			float matrix[16];
 			uint8_t color[4];
+			Element() {
+				matrix[0]=1;
+				matrix[1]=0;
+				matrix[2]=0;
+				matrix[3]=0;
+
+				matrix[4]=0;
+				matrix[5]=1;
+				matrix[6]=0;
+				matrix[7]=0;
+
+				matrix[8]=0;
+				matrix[9]=0;
+				matrix[10]=1;
+				matrix[11]=0;
+
+				matrix[12]=0;
+				matrix[13]=0;
+				matrix[14]=0;
+				matrix[15]=1;
+			};
 		};
 
 		AABB aabb;
