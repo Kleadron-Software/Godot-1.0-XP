19564057511f7c7fdf86054917ecc8d676770bfe
caimantilla <cmsCaiman@gmail.com>
2023-11-17 03:05:47 +0100

    add macos / osx editor build script (python 2 must be in path)

---
+++ tools/build_scripts/macos_editor_64.zsh
@@ -0,0 +1,13 @@
+echo "Number of threads to compile with: "
+read thread_count
+
+cd ../../
+
+python2 tools/scons/scons.py target=release_debug tools=yes musepack=no opus=yes platform=osx bits=64 optimized=speed -j$thread_count
+
+rm -rf "bin/godot_xp.app"
+cp -r "tools/Godot.app" ./"bin/godot_xp.app"
+mkdir -p "bin/godot_xp.app/Contents/MacOS"
+cp "bin/godot.osx.opt.tools.64" "bin/godot_xp.app/Contents/MacOS/Godot"
+chmod +x "bin/godot_xp.app/Contents/MacOS/Godot"
+
